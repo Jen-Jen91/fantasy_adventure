28b814975b3a0600bef43dae32f8c80afd693551
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-11-29 11:54:05 +0000

    Added Room, Treasure and Enemy classes

---
+++ src/main/java/fantasy/room/Treasure.java
@@ -0,0 +1,18 @@
+package fantasy.room;
+
+public class Treasure {
+    private String type;
+    private int value;
+
+    public Treasure(String type, int value){
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+    public int getValue(){
+        return this.value;
+    }
+}
